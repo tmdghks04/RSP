f17b9fa2a8bcdef80c6656f07227f49789983660
tmdghks04 <83343633+tmdghks04@users.noreply.github.com>
2022-05-05 13:14:30 +0900

    Add files via upload

---
+++ single.py
@@ -0,0 +1,89 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import random
+import time
+
+max_num_hands = 1
+gesture = {
+    0:'fist', 1:'one', 2:'two', 3:'three', 4:'four', 5:'five',
+    6:'six', 7:'rock', 8:'spiderman', 9:'yeah', 10:'ok',
+}
+rps_gesture = {0:'rock', 5:'paper', 9:'scissors'}
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+hands = mp_hands.Hands(
+    max_num_hands=max_num_hands,
+    min_detection_confidence=0.5,
+    min_tracking_confidence=0.5)
+
+file = np.genfromtxt('data/gesture_train.csv', delimiter=',')
+angle = file[:,:-1].astype(np.float32)
+label = file[:, -1].astype(np.float32)
+knn = cv2.ml.KNearest_create()
+knn.train(angle, cv2.ml.ROW_SAMPLE, label)
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3,1080)
+cap.set(4,720)
+while cap.isOpened():
+    ret, img = cap.read()
+    if not ret:
+        continue
+
+    img = cv2.flip(img, 1)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    result = hands.process(img)
+
+    img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+
+    if result.multi_hand_landmarks is not None:
+        for res in result.multi_hand_landmarks:
+            joint = np.zeros((21, 3))
+            for j, lm in enumerate(res.landmark):
+                joint[j] = [lm.x, lm.y, lm.z]
+#                print(lm.x, lm.y, lm.z)
+
+            
+            v1 = joint[[0,1,2,3,0,5,6,7,0,9,10,11,0,13,14,15,0,17,18,19],:]
+            v2 = joint[[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20],:]
+            v = v2 - v1
+            
+            v = v / np.linalg.norm(v, axis=1)[:, np.newaxis]
+
+            angle = np.arccos(np.einsum('nt,nt->n',
+                v[[0,1,2,4,5,6,8,9,10,12,13,14,16,17,18],:], 
+                v[[1,2,3,5,6,7,9,10,11,13,14,15,17,18,19],:]))
+#            print(angle)
+#            time.sleep(0.5)
+            
+            angle = np.degrees(angle)
+
+            data = np.array([angle], dtype=np.float32)
+            ret, results, neighbours, dist = knn.findNearest(data, 3)
+            idx = int(results[0][0])
+
+            if idx in rps_gesture.keys():
+                cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+            # Other gestures
+#                cv2.putText(img, text=gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
+#                robot_results = random.sample(Robot_list,1)
+#                print(robot_results)
+#                if rps_gesture[idx].upper() == 'paper':
+#                    if robot_results == 'Rock':
+#                        print('human win')
+#                    elif robot_results == 'Scissors':
+#                        print('You Lose')
+#                    else:
+#                        print('You Tied')
+
+#            time.sleep(0.5)# delay(1000)
+        mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
+            
+
+    cv2.imshow('Game', img)
+    if cv2.waitKey(1) == ord('q'):
+        break
