cce7ac762e238d4c6b2097cd0d2e03077233ab90
tmdghks04 <83343633+tmdghks04@users.noreply.github.com>
2022-05-09 18:48:57 +0900

    Update single.py

--- single.py
+++ single.py
@@ -1,7 +1,6 @@
 import cv2
 import mediapipe as mp
 import numpy as np
-import random
 import time
 
 max_num_hands = 1
@@ -18,7 +17,7 @@ hands = mp_hands.Hands(
     min_detection_confidence=0.5,
     min_tracking_confidence=0.5)
 
-file = np.genfromtxt('data/gesture_train.csv', delimiter=',')
+file = np.genfromtxt('gesture_train.csv', delimiter=',')
 angle = file[:,:-1].astype(np.float32)
 label = file[:, -1].astype(np.float32)
 knn = cv2.ml.KNearest_create()
@@ -28,6 +27,10 @@ cap = cv2.VideoCapture(0)
 
 cap.set(3,1080)
 cap.set(4,720)
+
+cTime = 0
+pTime = 0
+
 while cap.isOpened():
     ret, img = cap.read()
     if not ret:
@@ -39,7 +42,9 @@ while cap.isOpened():
     result = hands.process(img)
 
     img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-
+    cTime = time.time()
+    fps = 1/(cTime - pTime)
+    pTime = cTime 
     if result.multi_hand_landmarks is not None:
         for res in result.multi_hand_landmarks:
             joint = np.zeros((21, 3))
@@ -68,21 +73,14 @@ while cap.isOpened():
 
             if idx in rps_gesture.keys():
                 cv2.putText(img, text=rps_gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-            # Other gestures
+#             가위바위보 외에도 많은 제스처를 인식할 수 있음
 #                cv2.putText(img, text=gesture[idx].upper(), org=(int(res.landmark[0].x * img.shape[1]), int(res.landmark[0].y * img.shape[0] + 20)), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1, color=(255, 255, 255), thickness=2)
-#                robot_results = random.sample(Robot_list,1)
-#                print(robot_results)
-#                if rps_gesture[idx].upper() == 'paper':
-#                    if robot_results == 'Rock':
-#                        print('human win')
-#                    elif robot_results == 'Scissors':
-#                        print('You Lose')
-#                    else:
-#                        print('You Tied')
 
-#            time.sleep(0.5)# delay(1000)
+
+#            딜레이를 넣어서 실험할 때 사용           
+#            time.sleep(0.5) delay(1000)
         mp_drawing.draw_landmarks(img, res, mp_hands.HAND_CONNECTIONS)
-            
+    cv2.putText(img, text=str(int(fps)), org=(100,200), fontFace = cv2.FONT_HERSHEY_SIMPLEX, fontScale = 2, color=(0,0,0), thickness=2)         
 
     cv2.imshow('Game', img)
     if cv2.waitKey(1) == ord('q'):
